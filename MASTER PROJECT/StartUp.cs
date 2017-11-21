64453dbff359971d541f6e8e87ffc8b007b05258
VeselinovStf <chofexx@gmail.com>
2017-11-21 20:37:41 +0200

    Reformating StartUp

--- MASTER PROJECT/StartUp.cs
+++ MASTER PROJECT/StartUp.cs
@@ -19,16 +19,22 @@ public class StartUp
     {
         var identifiableInterface = typeof(Citizen).GetInterface("IIdentifiable");
         var birthableInterface = typeof(Citizen).GetInterface("IBirthable");
+		
         var properties = identifiableInterface.GetProperties();
+		
         Console.WriteLine(properties.Length);
         Console.WriteLine(properties[0].PropertyType.Name);
+		
         properties = birthableInterface.GetProperties();
+		
         Console.WriteLine(properties.Length);
         Console.WriteLine(properties[0].PropertyType.Name);
+		
         var name = Console.ReadLine();
         var age = int.Parse(Console.ReadLine());
         var id = Console.ReadLine();
         var birthdate = Console.ReadLine();
+		
         IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
         IBirthable birthable = new Citizen(name, age, id, birthdate);
     }
